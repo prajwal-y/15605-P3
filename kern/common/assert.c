ff1bf03eac31091f1fd7685e10fcb1310fac4ce2
Prajwal Y <prajwal299@gmail.com>
2015-11-20 04:01:58 -0500

    fix for fork_bomb()

--- kern/common/assert.c
+++ kern/common/assert.c
@@ -40,7 +40,6 @@ void panic(const char *fmt, ...)
     // exact authorship uncertain, popularized by Heinlein
     printf("When in danger or in doubt, run in circles, scream and shout.\n");
     lprintf("When in danger or in doubt, run in circles, scream and shout.");
-	MAGIC_BREAK;
     ++side_effect;
 }
 
@@ -59,7 +58,6 @@ void kernel_panic(const char *fmt, ...)
 	va_end(vl);
 	printf("\n");
 	lprintf("KERNEL_PANIC! Going to kill kernel!: %s", fmt);
-	MAGIC_BREAK;
     halt_handler();
 }
 
